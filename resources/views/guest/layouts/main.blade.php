03298d6f60e09bdddc1cb8dd1824f650e50468a3
arybobysiregar <aryboby1@gmail.com>
2022-12-15 15:52:55 +0700

    fix ui

--- resources/views/guest/layouts/main.blade.php
+++ resources/views/guest/layouts/main.blade.php
@@ -10,24 +10,20 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
 
-    <nav class="px-2 py-4 bg-secondaryColor text-white border-gray-200 ">
-        <div class="px-2 flex flex-row items-center justify-between mx-auto">
+    <nav class="px-2 py-4 bg-secondaryColor text-white border-gray-200  ">
+        <div class="px-2 flex flex-wrap items-center justify-between mx-auto">
             <a href="#" class="flex items-center">
-                <img src="{{ asset('assets/logo.png') }}" class="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
-                <span class="self-center text-white text-xl font-semibold whitespace-nowrap ">
-                    BPBD
-                    <span class="invisible md:visible">Provinsi Sumatera Utara </span>
-                </span>
+                <img src="{{asset('assets/logo.png')}}" class="h-6 mr-3 sm:h-9" alt=" Logo" />
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white flex gap-2">BPBD <span class="hidden md:block md:w-fit">Provinsi Sumatera Utara</span></span>
             </a>
             <button data-collapse-toggle="navbar-dropdown" type="button"
-                class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="inline-flex items-center p-2 ml-3 text-sm text-gray-500  rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                 aria-controls="navbar-dropdown" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
@@ -37,25 +33,25 @@
                         clip-rule="evenodd"></path>
                 </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto " id="navbar-dropdown">
+            <div class="hidden bg-secondaryColor w-full md:block md:w-auto " id="navbar-dropdown">
                 <ul
-                    class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
+                    class=" flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
+                    <li class="bg-primaryColor p-2 rounded-lg">
                         <a href="#"
-                            class="block py-2 pl-3 pr-4 text-white text-lg font-semibold rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
+                            class="block py-2 pl-3 pr-4 text-white text-lg font-semibold rounded md:bg-transparent md:text-blue-700 md:p-0 "
                             aria-current="page">Home</a>
                     </li>
-                    <li>
+                    <li class="p-2">
                         <a href="#"
-                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-lg font-semibold">Services</a>
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-lg font-semibold">Services</a>
                     </li>
-                    <li>
+                    <li class="p-2">
                         <a href="#"
-                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-lg font-semibold">Pricing</a>
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-gray-400 text-lg font-semibold">Pricing</a>
                     </li>
-                    <li>
+                    <li class="p-2">
                         <a href="#"
-                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-lg font-semibold">Contact</a>
+                            class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0  text-lg font-semibold">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -101,3 +97,5 @@
 </body>
 
 </html>
+
+<script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,42 +6,45 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js"
     ],
-
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
-        textColor:{
-            white:'#fff',
-            primaryColor:'#FF6501',
-            secondaryColor:'#051951',
-            softIndigoColor:'#F7F8FC',
-            indigoColor:'#5C6BC0',
-            softPinkColor:'#FFF7FA',
-            pinkColor:'#EC407A',
-            softGreenColor:'#F0FAFA',
-            greenColor:'#069697',
-            black:'#0000',
-            purple:'#C273FF',
+        textColor: {
+            white: '#fff',
+            primaryColor: '#FF6501',
+            secondaryColor: '#051951',
+            softIndigoColor: '#F7F8FC',
+            indigoColor: '#5C6BC0',
+            softPinkColor: '#FFF7FA',
+            pinkColor: '#EC407A',
+            softGreenColor: '#F0FAFA',
+            greenColor: '#069697',
+            black: '#0000',
+            purple: '#C273FF',
         },
-        colors:{
-            primaryColor:'#FF6501',
-            secondaryColor:'#051951',
-            softIndigoColor:'#F7F8FC',
-            indigoColor:'#5C6BC0',
-            softPinkColor:'#FFF7FA',
-            pinkColor:'#EC407A',
-            softGreenColor:'#F0FAFA',
-            greenColor:'#069697',
-            black:'#0000',
-            purple:'#C273FF',
-            white:'#fff',
-            blue:'#5D5FEF',
+        colors: {
+            primaryColor: '#FF6501',
+            secondaryColor: '#051951',
+            softIndigoColor: '#F7F8FC',
+            indigoColor: '#5C6BC0',
+            softPinkColor: '#FFF7FA',
+            pinkColor: '#EC407A',
+            softGreenColor: '#F0FAFA',
+            greenColor: '#069697',
+            black: '#0000',
+            purple: '#C273FF',
+            white: '#fff',
+            blue: '#5D5FEF',
         }
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        //  require('flowbite/plugin'), 
+        ],
 };
