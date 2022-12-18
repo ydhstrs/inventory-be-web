7236ad10395900184039c050b8442c046a008325
arybobysiregar <aryboby1@gmail.com>
2022-12-18 17:38:09 +0700

    add validation login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,12 +12,21 @@
 
                     <div class="p-10 px-20 flex flex-col items-center justify-center">
 
-
+                        @if ($errors->any())
+                        <div id="alert" class="alert mx-10 alert bg-softPinkColor rounded-lg py-5 px-6 mb-4 text-base text-pinkColor" role="alert">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
-                            <span class="text-secondaryColor font-bold text-3xl mb-4 text-center">Register Account</span>
+                            <span class="text-secondaryColor font-bold text-3xl mb-4 text-center">Register
+                                Account</span>
 
 
                             <!-- Name -->
@@ -34,6 +43,31 @@
                                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
                                     :value="old('email')" required />
                                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+
+                            <div class="mt-4">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">NIP</label>
+                                <input type="text" id="nip" name="nip"
+                                    class="form-control bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg block w-full p-2.5"
+                                    placeholder="" required>
+                            </div>
+                            <div class="mt-4">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">No Hp</label>
+                                <input type="text" id="no_hp" name="no_hp"
+                                    class="form-control bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg block w-full p-2.5"
+                                    placeholder="" required>
+                            </div>
+                            <div class="mt-4">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">
+                                    Kab/Kota
+                                </label>
+                                <select id="jenis_peralatan" name="id_kota"
+                                    class="bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                    <option selected>Pilih Kab/Kota</option>
+                                    @foreach ($kotas as $kota)
+                                        <option value="{{ $kota->id }}">{{ $kota->nama_kota }}</option>
+                                    @endforeach
+                                </select>
                             </div>
 
                             <!-- Password -->
