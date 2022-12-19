bd57d47b8d6c30634e700539fcd5c0ec7b1d10d5
arybobysiregar <aryboby1@gmail.com>
2022-12-20 00:06:13 +0700

    menu permintaan barang

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -164,6 +164,17 @@
                             </x-nav-link>
                         </li>
                         <li>
+                            <x-nav-link href="#" :active="request()->routeIs('admindistribusi.index')">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
+
+                                <span class="flex-1 ml-3 whitespace-nowrap">Permintaan Barang</span>
+                            </x-nav-link>
+                        </li>
+                        <li>
                             <x-nav-link href="{{ route('kategori.index') }}" :active="request()->routeIs('kategori.index')">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
