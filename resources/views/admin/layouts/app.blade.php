35d8fe220913a93f936c2eb8bca6b361b6007abe
arybobysiregar <aryboby1@gmail.com>
2022-12-20 13:31:11 +0700

    add some changes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -179,7 +179,7 @@
                         </li>
                         @if (Auth::user()->role == 2)
                             <li>
-                                <x-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('')">
+                                <x-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round"
